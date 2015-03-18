65c4dd5eaa86f923924513ceadabc51fe0b2789a
Wes Kendall <wes.kendall@ambition.com>
2015-03-18 20:08:12 +0000

    perms and perm levels are now updaed in the DB and are tested in one scenario

--- restraint/tests/update_restraint_db_tests.py
+++ restraint/tests/update_restraint_db_tests.py
@@ -1,16 +1,33 @@
 from django.test import TestCase
 
 from restraint.core import register_restraint_config
-from restraint.models import PermSet
+from restraint.models import PermSet, Perm, PermLevel
 from restraint.update_restraint_db import update_restraint_db
 
 
 class UpdateRestraintDbTest(TestCase):
     def test_full_update_scenario(self):
         register_restraint_config({
-            'perm_sets': ['global', 'restricted']
+            'perm_sets': ['global', 'restricted'],
+            'perms': [{
+                'name': 'can_edit_stuff',
+                'levels': [{
+                    'name': 'all_stuff',
+                }, {
+                    'name': 'some_stuff',
+                }],
+            }, {
+                'name': 'can_view_stuff',
+            }]
         })
         update_restraint_db()
 
         self.assertEquals(
             set(PermSet.objects.values_list('name', flat=True)), set(['global', 'restricted']))
+
+        self.assertEquals(
+            set(Perm.objects.values_list('name', flat=True)), set(['can_view_stuff', 'can_edit_stuff']))
+
+        self.assertEquals(
+            set(PermLevel.objects.values_list('name', 'perm__name')),
+            set([('all_stuff', 'can_edit_stuff'), ('some_stuff', 'can_edit_stuff'), ('', 'can_view_stuff')]))
--- restraint/update_restraint_db.py
+++ restraint/update_restraint_db.py
@@ -1,7 +1,7 @@
 """
 Functions for updating the restraing configuration in the database.
 """
-from restraint.models import PermSet
+from restraint.models import PermSet, Perm, PermLevel
 from restraint.core import get_restraint_config
 
 from manager_utils import sync
@@ -9,6 +9,8 @@ from manager_utils import sync
 
 def update_restraint_db():
     update_perm_sets()
+    update_perms()
+    update_perm_levels()
 
 
 def update_perm_sets():
@@ -17,3 +19,24 @@ def update_perm_sets():
     """
     perm_sets = get_restraint_config()['perm_sets']
     sync(PermSet.objects.all(), [PermSet(name=name) for name in perm_sets], ['name'])
+
+
+def update_perms():
+    """
+    Updates Perm models in the DB based on the restraint config.
+    """
+    perms = get_restraint_config()['perms']
+    sync(Perm.objects.all(), [Perm(name=perm['name']) for perm in perms], ['name'])
+
+
+def update_perm_levels():
+    """
+    Updates the PermLevel models in the DB based on the restraint config.
+    """
+    perms = get_restraint_config()['perms']
+    perm_objs = {p.name: p for p in Perm.objects.all()}
+    sync(PermLevel.objects.all(), [
+        PermLevel(perm=perm_objs[perm['name']], name=perm_level['name'])
+        for perm in perms
+        for perm_level in perm.get('levels', [{'name': ''}])
+    ], ['name', 'perm'])
