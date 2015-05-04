33d43f4299ce42fa06f6eb42d1b94ae8f2cfaf49
Wes Kendall <wes.kendall@ambition.com>
2015-05-04 19:29:21 +0000

    updated to 1.8 and dropped 1.6 support

--- docs/release_notes.rst
+++ docs/release_notes.rst
@@ -1,8 +1,14 @@
 Release Notes
 =============
 
+v0.3.0
+------
+
+* Added Django 1.8 support and dropped 1.6 support
+
 v0.2.1
 ------
+
 * Added Django 1.7 support
 
 v0.2.0
--- restraint/__init__.py
+++ restraint/__init__.py
@@ -3,4 +3,4 @@ from .version import __version__
 from .core import register_restraint_config, get_restraint_config, Restraint, update_restraint_db
 
 
-django_app_config = 'restraint.apps.RestraintConfig'
+default_app_config = 'restraint.apps.RestraintConfig'
